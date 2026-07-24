ba460ecc8b1f3b6c35c09a2e434c659ba94015ac
Danna Gutierrez <dnnm.gutierrez@gmail.com>
2026-07-24 19:17:17 -0400

    Renamed folder

--- data_viz_challenge.R
+++ data_viz_challenge.R
@@ -1,5 +1,1 @@
-#PacificDataVizChallenge
-
-library(readr)
-df <- read_csv("SPC-DF_CLIMATE_CHANGE-1.0-complete-2026-05-31 17-32-40.csv")
-df_unf <- read_csv("SPC-DF_CLIMATE_CHANGE1.0-filtered-2026-06-01 02-47-21.csv")
+#Fall2026 Data Challenge
